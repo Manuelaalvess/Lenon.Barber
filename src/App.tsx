4ebfcc0f767a727d35ca8456a533f203cb1decd0
Manuela Alves <manuelaalvesw@gmail.com>
2026-07-23 20:07:09 -0300

    feat: adiciona pagina de contato com mapa e horario

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Home from "./pages/Home";
 import Trabalho from "./pages/Trabalho";
+import Contato from "./pages/Contato";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/trabalho" element={<Trabalho />} />
+            <Route path="/contato" element={<Contato />} />
           </Routes>
         </AnimatePresence>
       </div>
