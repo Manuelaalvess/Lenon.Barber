a599f1ef0daba0d7466b3dbcbf87cc23949df8aa
Manuela Alves <manuelaalvesw@gmail.com>
2026-07-23 00:25:14 -0300

    feat: adiciona pagina de trabalho com servicos, galeria e faq

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Home from "./pages/Home";
+import Trabalho from "./pages/Trabalho";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
         <AnimatePresence mode="wait">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/trabalho" element={<Trabalho />} />
           </Routes>
         </AnimatePresence>
       </div>
